f66d9f46af29d8719095ed4a1dca1e1e7b521d7d
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-04-14 23:39:43 +0300

    social network project reforged

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var fs = require("fs")
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
+app.get('/profile', (req, res) => {
+  res.send("This is my Twi-Like page <a href='/1'> Main </a>")
+  
+})
+
+app.get('/1', (req, res) => {
   fs .appendFileSync("test.txt", req.query.message + "\n" + "<br>")
   var wall = fs.readFileSync("test.txt") 
   res.send(wall.toString() + "<br>" + "Author: " + req.query.author)
@@ -12,5 +17,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
 
 
